43ac15b24f18d9b76689bdb71524b1ff73c94729
jiangliu1 <1462547921@qq.com>
2021-10-20 16:49:55 +0800

    添加demo.html,关于防抖节流

--- dashboardimitate/src/components/HelloWorld.vue
+++ dashboardimitate/src/components/HelloWorld.vue
@@ -1,10 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
 
 <template>
   <h1>{{ msg }}</h1>
@@ -26,12 +19,29 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" @click="change">count is: {{ count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+const change = () => {
+  console.log(count);
+
+    return count
+
+}
+
+</script>
+
+
 
 <style scoped>
 a {
