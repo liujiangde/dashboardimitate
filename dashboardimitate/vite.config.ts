e2a80266c22f5bc6b04aadd39508836ba742e72a
jiangliu1 <1462547921@qq.com>
2021-12-08 10:18:22 +0800

    配置项目路径别名

--- dashboardimitate/vite.config.ts
+++ dashboardimitate/vite.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [vue(),
     Components({
       resolvers: [ElementPlusResolver()],
     }),
     ],
   resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "components": path.resolve(__dirname, "src/components"),
+      "styles": path.resolve(__dirname, "src/styles"),
+      "plugins": path.resolve(__dirname, "src/plugins"),
+      "views": path.resolve(__dirname, "src/views"),
+      "layouts": path.resolve(__dirname, "src/layouts"),
+      "utils": path.resolve(__dirname, "src/utils"),
+      "apis": path.resolve(__dirname, "src/apis"),
+      "dirs": path.resolve(__dirname, "src/directives"),
+    },
     extensions: [".ts", ".tsx", ".js"]
   },
 })
