ec6e4590b50835d8aadbde8099c1226c403af3c9
jiangliu1 <1462547921@qq.com>
2022-02-18 17:38:27 +0800

    插件配置

--- dashboardimitate/vite.config.ts
+++ dashboardimitate/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import path from 'path'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
@@ -15,6 +16,9 @@ export default defineConfig({
     }
   },
   plugins: [vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
     Components({
       resolvers: [ElementPlusResolver()]
     })
